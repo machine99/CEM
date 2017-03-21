457e4b18b1cd274872219d659c6381c2d7dd04aa
tomxie <tomxie1994@gmail.com>
2017-03-21 10:00:05 +0800

    修改所有个人信息，tomxie，2017年3月21日10:00:02

---
+++ src/main/java/com/baolian/dao/SysRoleDao.java
@@ -0,0 +1,14 @@
+package com.baolian.dao;
+
+import com.baolian.entity.SysRoleEntity;
+
+/**
+ * 角色管理
+ * 
+ *
+ *
+ * @date 2016年9月18日 上午9:33:33
+ */
+public interface SysRoleDao extends BaseDao<SysRoleEntity> {
+	
+}
